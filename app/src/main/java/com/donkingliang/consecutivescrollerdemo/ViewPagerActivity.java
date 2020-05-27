cdded3019ec77301d0139f2dd17a407a2ad5d382
teach <teachliang@can-dao.com>
2020-05-27 21:19:54 +0800

    处理ConsecutiveViewPager

--- app/src/main/java/com/donkingliang/consecutivescrollerdemo/ViewPagerActivity.java
+++ app/src/main/java/com/donkingliang/consecutivescrollerdemo/ViewPagerActivity.java
@@ -3,6 +3,7 @@ package com.donkingliang.consecutivescrollerdemo;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.donkingliang.consecutivescroller.ConsecutiveViewPager;
 import com.donkingliang.consecutivescrollerdemo.adapter.TabPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
@@ -11,9 +12,11 @@ import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
 public class ViewPagerActivity extends AppCompatActivity {
+
+    private ConsecutiveViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +28,17 @@ public class ViewPagerActivity extends AppCompatActivity {
                 "下面的例子中，通过自定义ViewPager，实现IConsecutiveScroller接口，ConsecutiveScrollerLayout能正确的处理ViewPager里" +
                 "的子布局。如果ViewPager的内容是可以垂直滑动的，请使用ConsecutiveScrollerLayout或者RecyclerView等可滑动布局作为它内容的根布局。\n" +
                 "下面的列子中使用ViewPager承载多个Fragment，Fragment的根布局为ConsecutiveScrollerLayout。");
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
         viewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager(), getTabs(), getFragments()));
         tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                viewPager.setAdjustHeight(tabLayout.getHeight());
+            }
+        });
     }
 
     private List<String> getTabs() {
--- consecutivescroller/src/main/java/com/donkingliang/consecutivescroller/ConsecutiveViewPager.java
+++ consecutivescroller/src/main/java/com/donkingliang/consecutivescroller/ConsecutiveViewPager.java
@@ -3,6 +3,7 @@ package com.donkingliang.consecutivescroller;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewParent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +19,47 @@ import androidx.viewpager.widget.ViewPager;
  */
 public class ConsecutiveViewPager extends ViewPager implements IConsecutiveScroller {
 
+    private int mAdjustHeight;
+
     public ConsecutiveViewPager(@NonNull Context context) {
         super(context);
     }
 
     public ConsecutiveViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (isAdjust() && mAdjustHeight > 0) {
+            ConsecutiveScrollerLayout layout = (ConsecutiveScrollerLayout) getParent();
+            int parentHeight = layout.getMeasuredHeight();
+            int height = Math.min(parentHeight - mAdjustHeight, getDefaultSize(0, heightMeasureSpec));
+            super.onMeasure(widthMeasureSpec,
+                    MeasureSpec.makeMeasureSpec(height, MeasureSpec.getMode(heightMeasureSpec)));
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    private boolean isAdjust() {
+        ViewParent parent = getParent();
+        if (parent instanceof ConsecutiveScrollerLayout) {
+            ConsecutiveScrollerLayout layout = (ConsecutiveScrollerLayout) parent;
+            return layout.indexOfChild(this) == layout.getChildCount() - 1;
+        }
+        return false;
+    }
+
+    public int getAdjustHeight() {
+        return mAdjustHeight;
+    }
+
+    public void setAdjustHeight(int adjustHeight) {
+        if (mAdjustHeight != adjustHeight) {
+            mAdjustHeight = adjustHeight;
+            requestLayout();
+        }
     }
 
     /**
@@ -56,8 +92,6 @@ public class ConsecutiveViewPager extends ViewPager implements IConsecutiveScrol
             for (int i = 0; i < count; i++) {
                 views.add(getChildAt(i));
             }
-        } else {
-            views.add(this);
         }
         return views;
     }
