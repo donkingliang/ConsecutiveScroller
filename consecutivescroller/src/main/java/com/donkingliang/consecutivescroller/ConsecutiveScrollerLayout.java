6e5cd9abe7ed144ced6583b408ed122f9137581c
teach <teachliang@can-dao.com>
2020-11-09 16:25:33 +0800

    同步主分支的一些修改。

--- consecutivescroller/src/main/java/com/donkingliang/consecutivescroller/ConsecutiveScrollerLayout.java
+++ consecutivescroller/src/main/java/com/donkingliang/consecutivescroller/ConsecutiveScrollerLayout.java
@@ -44,7 +44,7 @@ public class ConsecutiveScrollerLayout extends ViewGroup implements ScrollingVie
     /**
      * 联动容器可滚动的范围
      */
-    private int mScrollRange;
+    int mScrollRange;
 
     /**
      * 联动容器滚动定位子view
@@ -1272,7 +1272,7 @@ public class ConsecutiveScrollerLayout extends ViewGroup implements ScrollingVie
      *
      * @param target
      */
-    private void scrollChildContentToTop(View target) {
+    void scrollChildContentToTop(View target) {
         int scrollY = 0;
         do {
             scrollY = 0;
@@ -1290,7 +1290,7 @@ public class ConsecutiveScrollerLayout extends ViewGroup implements ScrollingVie
      *
      * @param target
      */
-    private void scrollChildContentToBottom(View target) {
+    void scrollChildContentToBottom(View target) {
         int scrollY = 0;
         do {
             scrollY = 0;
@@ -1375,7 +1375,7 @@ public class ConsecutiveScrollerLayout extends ViewGroup implements ScrollingVie
     /**
      * 停止滑动
      */
-    private void stopScroll() {
+    public void stopScroll() {
         mScroller.abortAnimation();
         stopNestedScroll(ViewCompat.TYPE_NON_TOUCH);
         if (mScrollToIndex == -1) {
